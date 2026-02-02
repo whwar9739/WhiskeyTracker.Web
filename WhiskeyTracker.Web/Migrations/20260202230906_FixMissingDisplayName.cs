c701094e2f461125572c7a6e60ad72616d93cacc
whwar9739 <ferrin.michael.84@gmail.com>
2026-02-02 17:10:58 -0600

    Fix: Add migration to safely restore missing DisplayName column in prod

---
+++ WhiskeyTracker.Web/Migrations/20260202230906_FixMissingDisplayName.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WhiskeyTracker.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixMissingDisplayName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.Sql(@"
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name='AspNetUsers' AND column_name='DisplayName') THEN
+        ALTER TABLE ""AspNetUsers"" ADD COLUMN ""DisplayName"" character varying(100);
+    END IF;
+END
+$$;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
